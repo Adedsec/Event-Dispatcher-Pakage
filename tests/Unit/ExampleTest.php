18ea0e75d4f62b50defafbcd04865c387e9d3698
dedsec <arefmaddah@gmail.com>
2021-02-22 23:54:39 +0330

    first commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function exampleTest()
+    {
+        $this->assertTrue(1 == 1);
+    }
+}
